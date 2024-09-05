7a6f925f5134c641fc0f2d26fa642841884737f4
xuxiaocheng <2762267080@qq.com>
2024-09-05 10:25:15 +0800

    docs(lib): 添加注释

--- src/check.rs
+++ src/check.rs
@@ -4,28 +4,54 @@ use std::io::Result;
 use bitflags::bitflags;
 
 bitflags! {
+    /// The strategy to check if the data is the same as the file.
     pub struct CheckStrategy: u8 {
         /// Always different.
         const Always = 1 << 0;
         /// Compare by file size.
         const Size = 1 << 1;
-    }
-}
-
-impl Default for CheckStrategy {
-    fn default() -> Self {
-        CheckStrategy::Size
+        // const First64Bytes = 1 << 2;
     }
 }
 
 impl CheckStrategy {
-    pub fn check_is_same(&self, data: &'static [u8], file: &File) -> Result<bool> {
+    /// The recommended strategy for configuration file. (always same and don't write)
+    #[inline]
+    pub fn config() -> Self {
+        Self::empty()
+    }
+
+    /// The recommended strategy for lite supported file. (always different and overwrite)
+    #[inline]
+    pub fn lite() -> Self {
+        Self::Always
+    }
+
+    /// The recommended strategy for heavy supported file. (just do necessary check)
+    #[inline]
+    pub fn heavy() -> Self {
+        // FIXME: replace if other strategies once available.
+        Self::Size
+    }
+}
+
+impl Default for CheckStrategy {
+    #[inline]
+    fn default() -> Self {
+        Self::lite()
+    }
+}
+
+impl CheckStrategy {
+    /// Return false only if it needs overwrite
+    pub(crate) fn check_file(&self, data: &'static [u8], file: &File) -> Result<bool> {
         if self.contains(CheckStrategy::Always) {
             return Ok(false);
         }
-        if self.contains(CheckStrategy::Size) && data.len() as u64 != file.metadata()?.len() {
+        let metadata = file.metadata()?;
+        if self.contains(CheckStrategy::Size) && data.len() as u64 != metadata.len() {
             return Ok(false);
         }
-        Ok(true)
+        Ok(metadata.len() > 0) // Check the file is not empty.
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,35 @@
-pub mod check;
+#![warn(missing_docs)]
+#![forbid(unsafe_code)]
+
+mod check;
 
 use std::fs::File;
 use std::io::{Result, Write};
 use std::path::Path;
 
-use crate::check::CheckStrategy;
+pub use crate::check::CheckStrategy;
 
+/// Write the embedded data to the file.
+///
+/// If the file already exists, it will be overwritten.
+///
+/// # Usage
+/// ```rust,ignore
+/// embedded_file(include_bytes!("data/file.txt"), "file.txt");
+/// ```
 pub fn embedded_file(data: &'static [u8], file: impl AsRef<Path>) -> Result<()> {
-    embedded_file_check(data, file, CheckStrategy::default())
+    embedded_file_check(data, file, CheckStrategy::Always)
 }
 
+/// Check if the file is the same as the embedded data, if not, write the embedded data to the file.
+///
+/// # Usage
+/// ```rust,ignore
+/// embedded_file_check(include_bytes!("data/file.txt"), "file.txt", CheckStrategy::lite());
+/// ```
 pub fn embedded_file_check(data: &'static [u8], file: impl AsRef<Path>, check: CheckStrategy) -> Result<()> {
     let mut file = File::options().read(true).write(true).create(true).open(file)?;
-    if check.check_is_same(data, &file)? {
+    if check.check_file(data, &file)? {
         Ok(())
     } else {
         file.write_all(data)
